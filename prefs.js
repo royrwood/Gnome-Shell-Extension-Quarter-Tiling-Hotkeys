9fc2ca9586f4ca36275fc56bbdfd67fcf0c48124
Roy Wood <roy.wood@gmail.com>
2023-05-07 10:38:07 -0400

    Cool-- it's almost right

--- prefs.js
+++ prefs.js
@@ -8,70 +8,87 @@ function init () {
 }
 
 
-function fillPreferencesWindow(window) {
+function fillPreferencesWindow(adwPreferencesWindow) {
+    let myExtensionSettings = ExtensionUtils.getSettings('org.gnome.shell.extensions.quarter-tiling-hotkeys');
 
-    settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.quarter-tiling-hotkeys');
+    const acceleratorKeysPrefPage = new Adw.PreferencesPage();
+    adwPreferencesWindow.add(acceleratorKeysPrefPage);
 
-    const preferencesPage = new Adw.PreferencesPage();
-    const preferencesGroup = new Adw.PreferencesGroup();
-    preferencesPage.add(preferencesGroup);
+    const acceleratorKeysPrefGroup = new Adw.PreferencesGroup();
+    acceleratorKeysPrefPage.add(acceleratorKeysPrefGroup);
 
-    // Factor this out so we can do all the accelerator keys...
-    const preferencesActionRow = new Adw.ActionRow({ title: 'Tile Left Accelerator Key' });
-    preferencesGroup.add(preferencesActionRow);
+    _addAcceleratorKeyPrefRow("Tile Left", "tile-left-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Right", "tile-right-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Up", "tile-up-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Down", "tile-down-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+}
 
-    let currentAcceleratorGtkButton = new Gtk.Button({ "label": '<Ctrl><Alt><Super>Left', "halign": Gtk.Align.CENTER, "valign": Gtk.Align.CENTER });
+
+function _addAcceleratorKeyPrefRow(promptText, acceleratorSettingName, myExtensionSettings, adwPreferencesGroup, adwPreferencesWindow) {
+    let currentAcceleratorKeySettingList = myExtensionSettings.get_strv(acceleratorSettingName);
+    log(`currentAcceleratorKeySettingList=${currentAcceleratorKeySettingList}`);
+    let currentAcceleratorKeySetting = currentAcceleratorKeySettingList[0];
+    log(`currentAcceleratorKeySetting=${currentAcceleratorKeySetting}`);
+
+    let newAcceleratorKeySetting = currentAcceleratorKeySetting;
+
+    const preferencesActionRow = new Adw.ActionRow({ title: promptText });
+    adwPreferencesGroup.add(preferencesActionRow);
+
+    let currentAcceleratorGtkButton = new Gtk.Button({ "label": currentAcceleratorKeySetting, "halign": Gtk.Align.CENTER, "valign": Gtk.Align.CENTER });
     preferencesActionRow.add_suffix(currentAcceleratorGtkButton);
 
     currentAcceleratorGtkButton.connect('clicked', () => {
-        log(`The button was clicked`);
+        log(`The ${acceleratorSettingName} button was clicked`);
 
-        let gtkDialog = new Gtk.MessageDialog({"modal": true});
+        let gtkMessageDialog = new Gtk.MessageDialog({"modal": true});
 
-        gtkDialog.set_transient_for(window);
-        gtkDialog.add_button("OK", Gtk.ResponseType.OK);
-        gtkDialog.add_button("CANCEL", Gtk.ResponseType.CANCEL);
-        gtkDialog.text = "Choose keyboard accelerator";
-        gtkDialog.secondary_text = "<Control><Alt><Super>Left";
+        gtkMessageDialog.set_transient_for(adwPreferencesWindow);
+        gtkMessageDialog.add_button("OK", Gtk.ResponseType.OK);
+        gtkMessageDialog.add_button("CANCEL", Gtk.ResponseType.CANCEL);
+        gtkMessageDialog.text = "Choose keyboard accelerator";
+        gtkMessageDialog.secondary_text = currentAcceleratorKeySetting;
 
-        let gtkDialogMessageAreaBox = gtkDialog.get_message_area();
-
-        gtkDialog.connect("response", (dialog, response) => {
+        gtkMessageDialog.connect("response", (dialog, response) => {
+            // Gtk.ResponseType.DELETE_EVENT (escape -> gtkMessageDialog.close())
+            // Gtk.ResponseType.OK (OK button)
+            // Gtk.ResponseType.CANCEL (CANCEL button)
             log(`Got "response" signal: response=${response}`);
-            gtkDialog.destroy();
-        });
+            gtkMessageDialog.destroy();
 
-
-        let eventControllerKey = new Gtk.EventControllerKey();
-        gtkDialog.add_controller(eventControllerKey);
-
-        eventControllerKey.connect('key-pressed', (_widget, keyval, keycode, state) => {
-            log(`Key pressed: keyval=${keyval}, keycode=${keycode}, state=${state}`);
-
-            // let mask = state & Gtk.accelerator_get_default_mod_mask();
-
-            // if (mask === 0 && keyval === Gdk.KEY_Escape) {
-            //     dialogGtkWindow.close();
-            //     return Gdk.EVENT_STOP;
-            // }
-
-            // let binding = Gtk.accelerator_name_with_keycode(null, keyval, keycode, mask);
-            // log(`Got binding=${binding}`);
-            // currentAcceleratorKeySetting = binding;
-            // dialogAccelLabel.set_label(currentAcceleratorKeySetting);
+            if (response == Gtk.ResponseType.OK) {
+                log(`Got response Gtk.ResponseType.OK`);
+                currentAcceleratorKeySetting = newAcceleratorKeySetting;
+                currentAcceleratorGtkButton.set_label(currentAcceleratorKeySetting);
+            }
+            // Only set if user clicks "OK"
             // currentAcceleratorGtkButton.set_label(currentAcceleratorKeySetting)
-
             // settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.quarter-tiling-hotkeys');
             // settings.set_strv('tile-left-hotkey', [binding])
         });
 
-        
-        gtkDialog.present();
+        let eventControllerKey = new Gtk.EventControllerKey();
+        gtkMessageDialog.add_controller(eventControllerKey);
+
+        eventControllerKey.connect('key-pressed', (_widget, keyval, keycode, state) => {
+            log(`Key pressed: keyval=${keyval}, keycode=${keycode}, state=${state}`);
+
+            let mask = state & Gtk.accelerator_get_default_mod_mask();
+
+            if (mask === 0 && keyval === Gdk.KEY_Escape) {
+                gtkMessageDialog.close();
+                return Gdk.EVENT_STOP;
+            }
+
+            let binding = Gtk.accelerator_name_with_keycode(null, keyval, keycode, mask);
+            log(`Got binding=${binding}`);
+            newAcceleratorKeySetting = binding;
+            gtkMessageDialog.secondary_text = newAcceleratorKeySetting;
+        });
+
+        gtkMessageDialog.present();
     });
-
-    window.add(preferencesPage);
 }
-
 
 
 
@@ -171,55 +188,3 @@ function fillPreferencesWindow(window) {
 //         dialogGtkWindow.present();
 //     });
 // }
-
-
-
-    // let mainGtkBox = new Gtk.Box({ "height-request": 50, "width-request": 70, "orientation": Gtk.Orientation.VERTICAL });
-
-    // let mainGtkFrame = new Gtk.Frame({ "halign": Gtk.Align.CENTER, "margin-start": 25, "margin-end": 25, "margin-top": 25, "margin-bottom": 25 });
-    // mainGtkBox.append(mainGtkFrame);
-
-    // let mainGtkListBox = new Gtk.ListBox({ "selection-mode": Gtk.SelectionMode.NONE });
-    // mainGtkFrame.set_child(mainGtkListBox);
-
-    // _addAcceleratorKeySettingRow("Tile Left", "tile-left-hotkey", mainGtkListBox, settings);
-    // _addAcceleratorKeySettingRow("Tile Right", "tile-right-hotkey", mainGtkListBox, settings);
-    // _addAcceleratorKeySettingRow("Tile Up", "tile-up-hotkey", mainGtkListBox, settings);
-    // _addAcceleratorKeySettingRow("Tile Down", "tile-down-hotkey", mainGtkListBox, settings);
-
-    // return mainGtkBox;
-
-
-
-
-    // // Use the same GSettings schema as in `extension.js`
-    // const settings = ExtensionUtils.getSettings(
-    //     'org.gnome.shell.extensions.example');
-    
-    // // Create a preferences page and group
-    // const page = new Adw.PreferencesPage();
-    // const group = new Adw.PreferencesGroup();
-    // page.add(group);
-
-    // // Create a new preferences row
-    // const row = new Adw.ActionRow({ title: 'Show Extension Indicator' });
-    // group.add(row);
-
-    // // Create the switch and bind its value to the `show-indicator` key
-    // const toggle = new Gtk.Switch({
-    //     active: settings.get_boolean ('show-indicator'),
-    //     valign: Gtk.Align.CENTER,
-    // });
-    // settings.bind(
-    //     'show-indicator',
-    //     toggle,
-    //     'active',
-    //     Gio.SettingsBindFlags.DEFAULT
-    // );
-
-    // // Add the switch to the row
-    // row.add_suffix(toggle);
-    // row.activatable_widget = toggle;
-
-    // Add our page to the window
-    // window.add(page);
