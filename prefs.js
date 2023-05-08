9a48b338fe8493bcb26e0882a65fdf72763b8fc8
Roy Wood <roy.wood@gmail.com>
2023-05-08 09:30:13 -0400

    Cleanup code, add comments

--- prefs.js
+++ prefs.js
@@ -18,7 +18,9 @@ function init () {
 
 
 function fillPreferencesWindow(adwPreferencesWindow) {
-    let myExtensionSettings = ExtensionUtils.getSettings('org.gnome.shell.extensions.quarter-tiling-hotkeys');
+    let extensionGIOSettings = ExtensionUtils.getSettings('org.gnome.shell.extensions.quarter-tiling-hotkeys');
+
+    // Add a preferences page + group + rows for each accelerator key
 
     const acceleratorKeysPrefPage = new Adw.PreferencesPage();
     adwPreferencesWindow.add(acceleratorKeysPrefPage);
@@ -26,58 +28,66 @@ function fillPreferencesWindow(adwPreferencesWindow) {
     const acceleratorKeysPrefGroup = new Adw.PreferencesGroup();
     acceleratorKeysPrefPage.add(acceleratorKeysPrefGroup);
 
-    _addAcceleratorKeyPrefRow("Tile Left", "tile-left-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
-    _addAcceleratorKeyPrefRow("Tile Right", "tile-right-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
-    _addAcceleratorKeyPrefRow("Tile Up", "tile-up-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
-    _addAcceleratorKeyPrefRow("Tile Down", "tile-down-hotkey", myExtensionSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Left", "tile-left-hotkey", extensionGIOSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Right", "tile-right-hotkey", extensionGIOSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Up", "tile-up-hotkey", extensionGIOSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
+    _addAcceleratorKeyPrefRow("Tile Down", "tile-down-hotkey", extensionGIOSettings, acceleratorKeysPrefGroup, adwPreferencesWindow);
 }
 
 
-function _addAcceleratorKeyPrefRow(promptText, acceleratorSettingName, myExtensionSettings, adwPreferencesGroup, adwPreferencesWindow) {
-    let currentAcceleratorKeySettingList = myExtensionSettings.get_strv(acceleratorSettingName);
+function _addAcceleratorKeyPrefRow(acceleratorDescription, acceleratorSettingName, extensionGIOSettings, adwPreferencesGroup, adwPreferencesWindow) {
+    let currentAcceleratorKeySettingList = extensionGIOSettings.get_strv(acceleratorSettingName);
     _log(`currentAcceleratorKeySettingList=${currentAcceleratorKeySettingList}`);
     let currentAcceleratorKeySetting = currentAcceleratorKeySettingList[0];
     _log(`currentAcceleratorKeySetting=${currentAcceleratorKeySetting}`);
 
-    const preferencesActionRow = new Adw.ActionRow({ title: promptText });
+    // Add a preferences row with a label and a button showing the current setting
+    const preferencesActionRow = new Adw.ActionRow({ title: acceleratorDescription });
     adwPreferencesGroup.add(preferencesActionRow);
 
     let currentAcceleratorGtkButton = new Gtk.Button({ "label": currentAcceleratorKeySetting, "halign": Gtk.Align.CENTER, "valign": Gtk.Align.CENTER });
     preferencesActionRow.add_suffix(currentAcceleratorGtkButton);
 
-    currentAcceleratorGtkButton.connect('clicked', _chooseNewAcceleratorKey.bind(null, adwPreferencesWindow, currentAcceleratorGtkButton, promptText, myExtensionSettings, acceleratorSettingName));
+    // When the user clicks the button, allow them to choose a new accelerator key
+    currentAcceleratorGtkButton.connect('clicked', _chooseNewAcceleratorKey.bind(null, adwPreferencesWindow, currentAcceleratorGtkButton, acceleratorDescription, extensionGIOSettings, acceleratorSettingName));
 }
 
 
-function _chooseNewAcceleratorKey(adwPreferencesWindow, currentAcceleratorGtkButton, promptText, myExtensionSettings, acceleratorSettingName) {
+function _chooseNewAcceleratorKey(adwPreferencesWindow, currentAcceleratorGtkButton, acceleratorDescription, extensionGIOSettings, acceleratorSettingName) {
     _log(`The ${acceleratorSettingName} button was clicked`);
+
+    // Show a dialog to allow the user to pick a new accelerator key, or disable the action
 
     let gtkMessageDialog = new Gtk.MessageDialog({"modal": true});
 
     gtkMessageDialog.set_transient_for(adwPreferencesWindow);
-    gtkMessageDialog.add_button("OK", Gtk.ResponseType.OK);
-    gtkMessageDialog.add_button("DISABLE", 100);
+    gtkMessageDialog.add_button("ACCEPT", Gtk.ResponseType.OK);
+    gtkMessageDialog.add_button("DISABLE", 1234);  // We can use any return code greater than zero, so I choose 1234
     gtkMessageDialog.add_button("CANCEL", Gtk.ResponseType.CANCEL);
-    gtkMessageDialog.text = `Choose "${promptText}" keyboard accelerator`;
-    gtkMessageDialog.secondary_text = myExtensionSettings.get_strv(acceleratorSettingName)[0];
+    gtkMessageDialog.text = `Choose "${acceleratorDescription}" keyboard accelerator`;
+    gtkMessageDialog.secondary_text = currentAcceleratorGtkButton.label;
 
+    // When the dialog completes, do the right thing wrt the button the user clicked
     gtkMessageDialog.connect("response", (dialog, response) => {
         _log(`Got "response" signal: response=${response}`);
-        gtkMessageDialog.destroy();
 
-        if (response === 100) {
+        if (response === 1234) {
             _log(`Got response DISABLE`);
-            // TODO: Support disabling the hotkey-- set to [] or ["disabled"]
-            // https://gjs-docs.gnome.org/meta12~12/meta.display#method-add_keybinding
+            let newAcceleratorKeySetting = "disabled";
+            currentAcceleratorGtkButton.set_label(newAcceleratorKeySetting);
+            extensionGIOSettings.set_strv(acceleratorSettingName, [newAcceleratorKeySetting]);
         }
         else if (response === Gtk.ResponseType.OK) {
             _log(`Got response Gtk.ResponseType.OK`);
-            let newAcceleratorKeySetting = gtkMessageDialog.secondary_text;
+            let newAcceleratorKeySetting = gtkMessageDialog.secondary_text;  // The EventControllerKey stored the new binding in the secondary_text (see below)
             currentAcceleratorGtkButton.set_label(newAcceleratorKeySetting);
-            myExtensionSettings.set_strv(acceleratorSettingName, [newAcceleratorKeySetting]);
+            extensionGIOSettings.set_strv(acceleratorSettingName, [newAcceleratorKeySetting]);
         }
+
+        gtkMessageDialog.destroy();
     });
 
+    // Add an EventControllerKey so we can get keypress events in the dialog
     let eventControllerKey = new Gtk.EventControllerKey();
     gtkMessageDialog.add_controller(eventControllerKey);
 
@@ -86,15 +96,10 @@ function _chooseNewAcceleratorKey(adwPreferencesWindow, currentAcceleratorGtkBut
 
         let modifierKeys = state & Gtk.accelerator_get_default_mod_mask();
 
-        if (modifierKeys === 0 && keyval === Gdk.KEY_Escape) {
-            gtkMessageDialog.close();
-            return Gdk.EVENT_STOP;
-        }
-
         if (Gtk.accelerator_valid(keyval, modifierKeys)) {
             let binding = Gtk.accelerator_name_with_keycode(null, keyval, keycode, modifierKeys);
             _log(`Got binding=${binding}`);
-            gtkMessageDialog.secondary_text = binding;
+            gtkMessageDialog.secondary_text = binding; // The outer code will grab the new binding from here when the dialog closes (see above)
         }
     });
 
